1659fff574104ba3488ed1183624db35dd6ed975
Nibin-john <nibinjohn24@gmail.com>
2022-07-08 12:34:08 +0530

    shopapp-completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShopAppComponent } from './shop-app/shop-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShopAppComponent
   ],
   imports: [
     BrowserModule,
